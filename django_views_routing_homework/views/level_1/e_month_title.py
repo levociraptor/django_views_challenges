dc1078312f3338ab5f613e85a9a348c5b39190a6
levociraptor <leva.uspensky@gmail.com>
2024-09-15 22:45:07 +0400

    level_1 done

--- django_views_routing_homework/views/level_1/e_month_title.py
+++ django_views_routing_homework/views/level_1/e_month_title.py
@@ -14,9 +14,14 @@ from django.http import HttpResponse, HttpResponseNotFound
 
 
 def get_month_title_by_number(month_number: int):
-    pass  # код писать тут
+    months = ['January', 'February', 'March', 'April', 'May', 'June',
+              'July', 'August', 'September', 'October', 'November', 'December']
+
+    return months[month_number-1]
 
 
 def get_month_title_view(request, month_number: int):
-    # код писать тут
-    return HttpResponseNotFound('Месяца с таким номером не существует')
+    if 1 <= month_number <= 12:
+        return HttpResponse(get_month_title_by_number(month_number))
+    else:
+        return HttpResponseNotFound('Месяца с таким номером не существует')
